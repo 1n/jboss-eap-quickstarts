a063e432fef420d964f9e929d6a3bacd4fe9df4b
Vladislav Tkatchev <Vladislav_Tkatchev@epam.com>
2014-08-14 16:55:48 +0300

    testing #2

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Goodbye " + name + "(testing #1)!";
+        return "Goodbye " + name + "(testing #2)!";
     }
 
 }
